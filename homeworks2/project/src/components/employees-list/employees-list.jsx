834353b6a526fffa8f17c9f4efe9c31c85024e1a
«katipopovaI» <katipopova@mail.ru>
2023-02-21 11:25:23 +0300

    homeworks2

---
+++ homeworks2/project/src/components/employees-list/employees-list.jsx
@@ -0,0 +1,25 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+
+import "./employees-list.css";
+
+const EmployeesList = ({ data }) => {
+  // используем spread
+  const elements = data.map((item) => {
+    // return <EmployeesListItem {...item} />;
+    // аналогичная запись
+    // return <EmployeesListItem name={item.name} salary={item.salary}/>;
+
+    const { id, ...itemProps } = item;
+    return <EmployeesListItem key={id} {...itemProps} />;
+  });
+  return <ul className="app-list list-group">{elements}</ul>;
+};
+// return (
+//   // <ul className="app-list list-group">
+//   //   <EmployeesListItem name="John" salary={800} />
+//   //   <EmployeesListItem name="Alex" salary={3000} />
+//   //   <EmployeesListItem name="Carl" salary={5000} />
+//   // </ul>
+// );
+
+export default EmployeesList;
